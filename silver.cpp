0015949da33271cff7847a33d58455d30c384210
rohit4813 <ranjanvermarohit@gmail.com>
2015-07-16 00:55:28 +0530

    Create silver.cpp

---
+++ silver.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+int n;
+while(1){
+    scanf("%d",&n);
+    if(n==0)
+        break;
+        int x=log2(n);
+    printf("%d\n",x);
+
+
+}
+return 0;
+}
