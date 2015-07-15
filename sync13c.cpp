9bbb5f38d922311fdb1ed4ad02ebf4d5e4051ed7
rohit4813 <ranjanvermarohit@gmail.com>
2015-07-15 22:09:25 +0530

    Create sync13c.cpp

---
+++ sync13c.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+int main(){
+int t,a,b;
+scanf("%d",&t);
+while(t--){
+  scanf("%d%d",&a,&b);
+    if(a%2==0||b%2==0)
+        printf("Suresh\n");
+    else
+        printf("Ramesh\n");
+}
+
+
+}
