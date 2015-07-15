7d33883a9d35408e00a0636cf750269a5c4e3607
rohit4813 <ranjanvermarohit@gmail.com>
2015-07-16 00:17:47 +0530

    Create eights.cpp

---
+++ eights.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include <iostream>
+//freopen("filename.txt","r",stdin);
+using namespace std;
+
+int main(){
+int t;
+long long int n;
+scanf("%d",&t);
+while(t--){
+scanf("%lld",&n);
+printf("%lld\n",(n-1)*250 + 192);
+}
+}
